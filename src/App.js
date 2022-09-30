a5e99ccc25a14676f33676a85e063e81cb2cf362
madmutt <3705987+madmutt@users.noreply.github.com>
2022-09-30 09:07:31 +0100

    added photowall slideshow

--- src/App.js
+++ src/App.js
@@ -3,11 +3,20 @@ import logo from './logo.png';
 import './App.css';
 
 function App() {
+    //Show/hide subreddits
+    const toggleAside = () => {
+        const el = document.getElementById("aside");
+        el.classList.contains("visible") === true ? el.classList.remove("visible") : el.classList.add("visible");
+    }
+    const getPosts = () => {
+        toggleAside();
+    }
+    //PhotoWall
     const togglePhotoWall = () => {
         const photoWall = document.getElementById("photoWall");
         const photoWallButton = document.getElementById("photoWallButton");
         const posts = document.getElementById("posts");
-
+        //check if photoWall is visible
         if(photoWall.offsetParent === null){
             posts.style.display = "none";
             photoWallButton.innerHTML = "Posts";
@@ -19,16 +28,51 @@ function App() {
             posts.style.display = "block";
         }
     }
+    //fullscreen slideshow
+    let slideIndex = 0;
+    let slides;
+    const showSlides = (n) => {
+        if(n >= slides.length){
+            slideIndex = 0;
+        }
+        if(n < 0){
+            slideIndex = slides.length-1;
+        }
+        document.getElementById('currentImg').src = slides[slideIndex].src;
+    }
+    const showSlideShow = (e) => {
+        //set clicked img as first slide
+        document.getElementById('currentImg').src = e.target.src;
+        //get index of clicked image
+        slides = Array.from(document.getElementById('photos').children);
+        slideIndex = slides.findIndex(img => img.src === e.target.src);
+        //show slide container
+        const slide = document.getElementById("slide");
+        slide.style.opacity = '1';
+        slide.style.zIndex = '100';
+        slide.style.left = '0';
+        slide.style.top = '0';
+        slide.style.width = '100%';
+        slide.style.height = '100%';
+    }
+    const hideSlideShow = () => {
+        const slide = document.getElementById("slide");
+        slide.style.opacity = '0';
+        slide.style.zIndex = '-1';
+        slide.style.left = '50%';
+        slide.style.top = '50%';
+        slide.style.width = '0';
+        slide.style.height = '0';
+    }
+    const showNextSlide = () => {
+        showSlides(slideIndex += 1);
+    }
+    const showPrevSlide = () => {
+        showSlides(slideIndex += -1);
+    }
 
-    const toggleAside = () => {
-        const el = document.getElementById("aside");
-        el.classList.contains("visible") === true ? el.classList.remove("visible") : el.classList.add("visible");
-    }
-    const getPosts = () => {
-        toggleAside();
-    }
     return (
-        <div class="wrapper">
+        <div className="wrapper">
             <header>
                 <button className="aside-toggle" type="button" onClick={toggleAside}></button>
                 <h1><img src={logo} className="logo" alt="logo" />Sub<span>reddit</span>Reader</h1>
@@ -47,25 +91,33 @@ function App() {
                 <section id="photoWall">
                 <h2>Photo Wall</h2>
                     <section id="photos">
-                        <img src="https://loremflickr.com/250/300" alt="" />
-                        <img src="https://loremflickr.com/350/350" alt="" />
-                        <img src="https://loremflickr.com/250/300" alt="" />
-                        <img src="https://loremflickr.com/350/350" alt="" />
-                        <img src="https://loremflickr.com/150/150" alt="" />
-                        <img src="https://loremflickr.com/250/300" alt="" />
-                        <img src="https://loremflickr.com/150/150" alt="" />
-                        <img src="https://loremflickr.com/250/300" alt="" />
-                        <img src="https://loremflickr.com/350/350" alt="" />
-                        <img src="https://loremflickr.com/250/300" alt="" />
-                        <img src="https://loremflickr.com/350/350" alt="" />
-                        <img src="https://loremflickr.com/150/150" alt="" />
-                        <img src="https://loremflickr.com/250/300" alt="" />
-                        <img src="https://loremflickr.com/350/350" alt="" />
-                        <img src="https://loremflickr.com/350/350" alt="" />
-                        <img src="https://loremflickr.com/150/150" alt="" />
-                        <img src="https://loremflickr.com/250/300" alt="" />
-                        <img src="https://loremflickr.com/350/350" alt="" />
+                        <img onClick={showSlideShow} src="https://loremflickr.com/2510/3000" alt="" />
+                        <img onClick={showSlideShow} src="https://loremflickr.com/3520/3500" alt="" />
+                        <img onClick={showSlideShow} src="https://loremflickr.com/2530/3000" alt="" />
+                        <img onClick={showSlideShow} src="https://loremflickr.com/3540/3500" alt="" />
+                        <img onClick={showSlideShow} src="https://loremflickr.com/1550/1500" alt="" />
+                        <img onClick={showSlideShow} src="https://loremflickr.com/2560/3000" alt="" />
+                        <img onClick={showSlideShow} src="https://loremflickr.com/1570/1500" alt="" />
+                        <img onClick={showSlideShow} src="https://loremflickr.com/2580/3000" alt="" />
+                        <img onClick={showSlideShow} src="https://loremflickr.com/3590/3500" alt="" />
+                        <img onClick={showSlideShow} src="https://loremflickr.com/2500/3010" alt="" />
+                        <img onClick={showSlideShow} src="https://loremflickr.com/3500/3520" alt="" />
+                        <img onClick={showSlideShow} src="https://loremflickr.com/1500/1530" alt="" />
+                        <img onClick={showSlideShow} src="https://loremflickr.com/2500/3040" alt="" />
+                        <img onClick={showSlideShow} src="https://loremflickr.com/3500/3550" alt="" />
+                        <img onClick={showSlideShow} src="https://loremflickr.com/3500/3560" alt="" />
+                        <img onClick={showSlideShow} src="https://loremflickr.com/1500/1570" alt="" />
+                        <img onClick={showSlideShow} src="https://loremflickr.com/2500/3080" alt="" />
+                        <img onClick={showSlideShow} src="https://loremflickr.com/3500/3590" alt="" />
                     </section>
+                    <div id="slide">
+                        <div className="img-wrapper">
+                            <p onClick={hideSlideShow} className="btnclose">close</p>
+                            <p onClick={showPrevSlide} className="prev">prev</p>
+                            <p onClick={showNextSlide} className="next">next</p>
+                            <img id="currentImg" src="https://loremflickr.com/2500/1646" alt="" />
+                        </div>
+                    </div>
                 </section>
             </article>
             <aside id="aside" className="aside visible">
@@ -79,7 +131,7 @@ function App() {
                 </ul>
             </aside>
             <footer className="footer">
-            <p>&copy; NotReddit 2022</p>
+                <p>&copy; NotReddit 2022</p>
             </footer>
         </div>
     );
